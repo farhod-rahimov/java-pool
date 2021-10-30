f6ce0ce124df3863f30e9090008d2789f1403c2e
Rahimov Farhod <farhod.rahimov0055@gmail.com>
2021-10-30 18:04:04 +0300

    ex00 done

---
+++ day-05/ex00/src/main/java/edu/school21/chat/models/Chatroom.java
@@ -0,0 +1,73 @@
+package edu.school21.chat.models;
+
+import java.util.List;
+import java.util.Objects;
+
+public class Chatroom {
+    private static  long            lastId = 1;
+    private         Long            id;
+    private         String          name;
+    private         User            owner;
+    private         List<Message>   messages;
+
+    public Chatroom(String name, User owner) {
+        this.id = Chatroom.lastId++;
+        this.name = name;
+        this.owner = owner;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public User getOwner() {
+        return owner;
+    }
+
+    public List<Message> getMessages() {
+        return messages;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setOwner(User owner) {
+        this.owner = owner;
+    }
+
+    public void setMessages(List<Message> messages) {
+        this.messages = messages;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Chatroom)) return false;
+        Chatroom chatroom = (Chatroom) o;
+        return id.equals(chatroom.id) && name.equals(chatroom.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
+
+    @Override
+    public String toString() {
+        return "Chatroom{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", owner=" + owner +
+                ", messages=" + messages +
+                '}';
+    }
+}
---
+++ day-05/ex00/src/main/resources/schema.sql
@@ -0,0 +1,35 @@
+create database ex00;
+
+-- \c ex00;
+-- first create database ex00 and then connect to it
+
+CREATE TABLE users (
+    id BIGSERIAL PRIMARY KEY NOT NULL,
+    login VARCHAR(100) UNIQUE NOT NULL,
+    password VARCHAR(100) NOT NULL);
+  
+CREATE TABLE chat_rooms (
+    id BIGSERIAL PRIMARY KEY NOT NULL,
+    name VARCHAR(100) NOT NULL,
+    owner BIGSERIAL REFERENCES users(id) NOT NULL);
+
+CREATE TABLE messages (
+    id BIGSERIAL PRIMARY KEY NOT NULL,
+    author BIGSERIAL REFERENCES users(id),
+    room BIGSERIAL REFERENCES chat_rooms(id),
+    text text NOT NULL,
+    date timestamp NOT NULL);
+
+CREATE TABLE chat_rooms_of_users (
+    user_id BIGSERIAL REFERENCES users(id) NOT NULL,
+    chat_room_id BIGSERIAL REFERENCES chat_rooms(id) NOT NULL);
+
+-- -- КТО В КАКИХ ГРУППАХ СОСТОИТ
+-- SELECT u.id as user_id, u.login, cr.name as chat_room_name, cr.id as chat_room_id, cr.owner as owner_id FROM users u
+-- left join chat_rooms_of_users crou on crou.user_id=u.id
+-- left join chat_rooms cr on cr.id=crou.chat_room_id
+
+-- СПИСОК ВСЕХ СООБЩЕНИЙ В КАЖДОЙ ГРУППЕ
+-- SELECT u.id as user_id, u.login, cr.name, m.text, m.date, cr.owner  FROM users u
+-- left join messages m on u.id = m.author
+-- left join chat_rooms cr on cr.id=m.room  ORDER BY u.login;
